31f8864fb943833c33ed53df09e0324a183a64ec
gingermadfire <Maxim.ilian98@yandex.ru>
2023-04-07 12:27:27 +0300

    Add modal for order goods

---
+++ src/app/goods-order-modal/goods-order-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goods-order-modal',
+  templateUrl: './goods-order-modal.component.html',
+  styleUrls: ['./goods-order-modal.component.css']
+})
+export class GoodsOrderModalComponent {
+
+}
